dfc819e2ca212079bb7ad3acaaf9c7722b2e3c59
BrodyNics77 <brodynichols777@gmail.com>
2024-09-07 04:14:45 -0500

    Test: Refactored test to take optinal argument

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,11 @@
 class Cell
 
-    attr_reader :coordinate, :ship
+    attr_reader :coordinate, :ship, :hit
 
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @hit = false
     end
 
     def empty?
@@ -12,6 +13,37 @@ class Cell
     end
 
     def place_ship(ship)
-        @ship = ship
+        if @ship == nil
+            @ship = ship
+        else
+            'cell is taken'
+        end
+    end
+    
+    def fired_upon?
+        @hit
+    end
+
+    def fire_upon
+        if fired_upon? == false
+            @hit = true
+            if @ship != nil
+                @ship.hit
+            end
+        else
+            "already fired upon"
+        end
+    end
+
+    def render(reveal = false)
+        if @hit == false
+            '.'
+        elsif @ship == nil
+            'M'
+        elsif @ship.sunk? != true
+            "H"
+        else
+            'X'
+        end
     end
 end
