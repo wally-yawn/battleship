60e4650e1e3fa4686630aa2ab9bc8236b1988917
BrodyNics77 <brodynichols777@gmail.com>
2024-09-08 21:21:40 -0500

    Test & Feat: Added Test & Location hash method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
+
 RSpec.describe Board do
     before (:each) do
-    @board = Board.new
+        @board = Board.new
     end
 
     it 'exist' do
-        expect(@board).to eq(Board)
+        expect(@board).to be_a(Board)
+    end
+
+    it 'has cells inside of a hash' do
+        expect(@board.cells).to be_a(Hash)
     end
 end
