23282b7e0591aa7c30b4151ee6220efe3c205f7a
BrodyNics77 <brodynichols777@gmail.com>
2024-09-09 00:53:52 -0500

    Test: Added test for #consecutive_checker

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe Board do
             expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
         end
     end
+
+    describe '#consecutive checker' do
+        it 'checks for consecutive' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2) 
+
+            expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq (false)
+            expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+            expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+            expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+        end
+    end
 end
