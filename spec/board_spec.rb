a3aafec18d3946cad9fff90001a998ec57e7b633
BrodyNics77 <brodynichols777@gmail.com>
2024-09-09 00:57:22 -0500

    Test: Added Test for Diagonal checker needs work

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -48,4 +48,13 @@ RSpec.describe Board do
             expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
         end
     end
+
+    describe '#diagonal_checker' do
+        it 'checks if a ship is trying to be placed diagonally' do
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2) 
+
+            
+        end
+    end
 end
